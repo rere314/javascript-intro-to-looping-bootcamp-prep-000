5c2c3832537a23c15976476ee6b907f2b8970a51
tlrollins9 <tlrollins9@gmail.com>
2020-06-28 04:27:46 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -25,5 +25,5 @@ function incrementVariable() {
 function doWhileLoop(n) {
   do { 
     console.log("I run once regardless.") 
-  } while (incrementVariable() <= (n);
+  } while (incrementVariable() <= (n));
 }
